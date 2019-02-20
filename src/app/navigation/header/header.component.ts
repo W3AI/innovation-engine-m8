45fcb11d2b06d47c4a10db80624298c6a40caf52
stefian <v_ianta@yahoo.com>
2019-02-20 14:59:49 -0500

    rewording swarm 4 fun and swarm for money

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -16,7 +16,7 @@ import { SetupService } from '../../setup.service';
 export class HeaderComponent implements OnInit {
   @Output() sidenavToggle = new EventEmitter<void>();
   isAuth$: Observable<boolean>;
-  appTitle = 'weSwarm';
+  appTitle = 'weSwarm 4 fun';
 
   constructor(private store: Store<fromRoot.State>, 
               private authService: AuthService,
