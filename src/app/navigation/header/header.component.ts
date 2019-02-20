70118e0ed31668891cc7f21b212531623eb1dda8
stefian <v_ianta@yahoo.com>
2019-02-20 11:01:16 -0500

    pivot to weSwarm 4 fun days weekend

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -16,7 +16,7 @@ import { SetupService } from '../../setup.service';
 export class HeaderComponent implements OnInit {
   @Output() sidenavToggle = new EventEmitter<void>();
   isAuth$: Observable<boolean>;
-  appTitle = 'weBrain';
+  appTitle = 'weSwarm';
 
   constructor(private store: Store<fromRoot.State>, 
               private authService: AuthService,
