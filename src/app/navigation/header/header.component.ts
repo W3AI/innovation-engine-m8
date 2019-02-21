7c20449470dc33350477ae371487f4da1e35d588
stefian <v_ianta@yahoo.com>
2019-02-21 15:40:08 -0500

    changed title to mob 6 to support genetic OS idea

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -16,7 +16,7 @@ import { SetupService } from '../../setup.service';
 export class HeaderComponent implements OnInit {
   @Output() sidenavToggle = new EventEmitter<void>();
   isAuth$: Observable<boolean>;
-  appTitle = 'weSwarm 4 fun';
+  appTitle = 'mob 6';
 
   constructor(private store: Store<fromRoot.State>, 
               private authService: AuthService,
