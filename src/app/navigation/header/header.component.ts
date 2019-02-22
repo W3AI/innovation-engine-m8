95034e79ac0c0b1941b37023cce678fd86161860
stefian <v_ianta@yahoo.com>
2019-02-22 11:27:54 -0500

    pivot to fun OS kernel theme and icons

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -16,7 +16,7 @@ import { SetupService } from '../../setup.service';
 export class HeaderComponent implements OnInit {
   @Output() sidenavToggle = new EventEmitter<void>();
   isAuth$: Observable<boolean>;
-  appTitle = 'fun mob';
+  appTitle = 'fun OS';
 
   constructor(private store: Store<fromRoot.State>, 
               private authService: AuthService,
