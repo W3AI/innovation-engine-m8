08a26d1a52adfc6dd31f4c11358bf54bc2778e6e
stefian <v_ianta@yahoo.com>
2019-02-25 13:48:15 -0500

    changed titles to free world and fair world

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -16,7 +16,7 @@ import { SetupService } from '../../setup.service';
 export class HeaderComponent implements OnInit {
   @Output() sidenavToggle = new EventEmitter<void>();
   isAuth$: Observable<boolean>;
-  appTitle = 'worldbook';
+  appTitle = 'fair world';
 
   constructor(private store: Store<fromRoot.State>, 
               private authService: AuthService,
