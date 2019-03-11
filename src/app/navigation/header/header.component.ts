b795d424c6c454099f6f3953af243d7878dcd061
stefian <v_ianta@yahoo.com>
2019-03-11 15:06:15 -0300

    added top leaders pics for wall street theme

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -16,7 +16,7 @@ import { SetupService } from '../../setup.service';
 export class HeaderComponent implements OnInit {
   @Output() sidenavToggle = new EventEmitter<void>();
   isAuth$: Observable<boolean>;
-  appTitle = 'walls street';
+  appTitle = 'wall street';
 
   constructor(private store: Store<fromRoot.State>, 
               private authService: AuthService,
