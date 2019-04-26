e8cf542c4455d7b863de3ae1ceff0ebad1cf2fc6
stefian <v_ianta@yahoo.com>
2019-04-26 17:01:47 -0400

    aiMash button

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -16,7 +16,7 @@ import { SetupService } from '../../setup.service';
 export class HeaderComponent implements OnInit {
   @Output() sidenavToggle = new EventEmitter<void>();
   isAuth$: Observable<boolean>;
-  appTitle = 'aiMash';
+  appTitle = 'Mash';
 
   constructor(private store: Store<fromRoot.State>, 
               private authService: AuthService,
