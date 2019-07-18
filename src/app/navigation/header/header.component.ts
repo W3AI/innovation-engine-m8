fbb132bdf4389e8224787f278acc3ea6dcf0e64e
stefian <stefan.ianta@w3ai.net>
2019-07-18 10:02:36 -0400

    ai/rbc title and logo

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -16,7 +16,7 @@ import { SetupService } from '../../setup.service';
 export class HeaderComponent implements OnInit {
   @Output() sidenavToggle = new EventEmitter<void>();
   isAuth$: Observable<boolean>;
-  appTitle = 'smart';
+  appTitle = 'ai';
 
   constructor(private store: Store<fromRoot.State>, 
               private authService: AuthService,
